fe517d58e4b60bd4535ae013d2a6848f6583d4b9
Revan Sarbast <revansarbast8@gmail.com>
2020-06-12 22:20:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ document.addEventListener('DOMContentLoaded',()=>{fetch(imgUrl).then(response=>r
   <img src="${data.message[1]}" alt="img2">
   <img src="${data.message[2]}" alt="img3">
   <img src="${data.message[3]}" alt="img4">`
-}).catch(err=>{console.log(err)})
+}).catch(err=>{console.log(err)});
+
 fetch(breedUrl).then(response=>response.json()).then(data=>{console.log(data)
-// let container = document.getElementById('dog-breeds')
-//   container.innerHTML=`
-//   <li><img src="${data.message[0]}" alt="img1"></li>
-//   <li><img src="${data.message[1]}" alt="img2"></li>
-//   <li><img src="${data.message[2]}" alt="img3"></li>
-//   <li><img src="${data.message[3]}" alt="img4"></li>`
+let container = document.getElementById('dog-breeds')
+  container.innerHTML=`
+  <li><img src="${data.message[0]}" alt="img1"></li>
+  <li><img src="${data.message[1]}" alt="img2"></li>
+  <li><img src="${data.message[2]}" alt="img3"></li>
+  <li><img src="${data.message[3]}" alt="img4"></li>`
 }).catch(err=>{console.log(err)})})
