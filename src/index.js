ef9ceb0943bff9ecabe7ff68cd0b4a4074653a31
Revan Sarbast <revansarbast8@gmail.com>
2020-06-12 22:23:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-document.addEventListener('DOMContentLoaded',()=>{fetch(imgUrl).then(response=>response.json()).then(data=>{console.log(data)
-  let container = document.getElementById('dog-image-container')
-  container.innerHTML=`<img src="${data.message[0]}" alt="img1">
-  <img src="${data.message[1]}" alt="img2">
-  <img src="${data.message[2]}" alt="img3">
-  <img src="${data.message[3]}" alt="img4">`
-}).catch(err=>{console.log(err)});
-
+document.addEventListener('DOMContentLoaded',()=>{
 })
 fetch(breedUrl).then(response1=>response1.json()).then(data1=>{console.log(data1)
 let container1 = document.getElementById('dog-breeds')
@@ -18,3 +11,10 @@ let container1 = document.getElementById('dog-breeds')
   <li><img src="${data1.message[2]}" alt="img6"></li>
   <li><img src="${data1.message[3]}" alt="img7"></li>`
 }).catch(err1=>{console.log(err1)})
+fetch(imgUrl).then(response=>response.json()).then(data=>{console.log(data)
+  let container = document.getElementById('dog-image-container')
+  container.innerHTML=`<img src="${data.message[0]}" alt="img1">
+  <img src="${data.message[1]}" alt="img2">
+  <img src="${data.message[2]}" alt="img3">
+  <img src="${data.message[3]}" alt="img4">`
+}).catch(err=>{console.log(err)});
