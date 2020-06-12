021f8707809e014d97976a0a03145b27a25ecc84
Revan Sarbast <revansarbast8@gmail.com>
2020-06-12 22:10:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 document.addEventListener('DOMContentLoaded',()=>{fetch(imgUrl).then(response=>response.json()).then(data=>{console.log(data)
   let container = document.getElementById('dog-image-container')
-  container.innerHTML=`<img src="${data.messege[0]}" alt="img1">`
+  container.innerHTML=`<img src="${data.message[0]}" alt="img1">`
 }).catch(err=>{console.log(err)})})
